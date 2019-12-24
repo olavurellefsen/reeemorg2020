6b54886ad000670c54e00bb61f774194cc0d7dee
olavurellefsen <oe@tokni.com>
2019-12-24 09:46:43 +0000

    Initial menu

---
+++ src/components/menu.js
@@ -0,0 +1,72 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+const Menu = ({ menuLinks }) => (
+  <NavStyle>
+    <MenuListStyle>
+      {menuLinks.map(link => (
+        <MenuItemStyle key={link.name}>
+          <LinkStyle to={link.link}>{link.name}{" "}<FontAwesomeIcon icon={faCaretDown} /></LinkStyle>
+        </MenuItemStyle>
+      ))}
+    </MenuListStyle>
+  </NavStyle>
+)
+
+Menu.propTypes = {
+  menuLinks: PropTypes.array,
+}
+
+Menu.defaultProps = {
+  menuLinks: [
+    { name: "About", link: "/objectives" },
+    { name: "Output", link: "/deliverables-by-theme" },
+    { name: "EMP-E", link: "/emp-e-2019" },
+    { name: "Concept", link: "/pathways" },
+    { name: "Tools", link: "/osemosys" },
+    { name: "News & Blog", link: "/sample-page" },
+    { name: "Contact", link: "/contact-us" },
+  ],
+}
+
+const NavStyle = styled.nav`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background: rgb(51, 54, 59);
+  padding: 0px 20px;
+  list-style: none;
+`
+
+const MenuListStyle = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background: #0a0a0a;
+  width: 100vw;
+  padding: 0px 15px;
+  margin: 0;
+  list-style: none;
+`
+
+const MenuItemStyle = styled.li`
+  font-size: 1rem;
+  display: inline-block;
+  margin: 0;
+  padding: 15px 14px;
+`
+
+const LinkStyle = styled(props => <Link {...props} />)`
+  color: rgba(255, 255, 255, 0.7);
+  line-height: 1.25rem;
+  text-decoration: none;
+  &:hover {
+    color: rgba(255, 255, 255, 1);
+  }
+`
+
+export default Menu
