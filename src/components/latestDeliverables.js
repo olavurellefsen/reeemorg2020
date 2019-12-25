5893c1a06b4d0434c91d86ebbf592fad9c5bb68e
olavurellefsen <oe@tokni.com>
2019-12-25 15:45:48 +0000

    Add latest deliverables to left menu

---
+++ src/components/latestDeliverables.js
@@ -0,0 +1,69 @@
+export default [
+  {
+    name: "Integrated Impact report",
+    link: "https://zenodo.org/record/3403800#.XX9AeZNKhTE",
+  },
+  {
+    name: "Policy Brief",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D1.3b_Second-Policy-Brief.pdf",
+  },
+  {
+    name: "Focus Report on economic impacts",
+    link: "http://www.reeem.org/wp-content/uploads/2019/08/D3.1b_Focus-Report-on-economic-impacts.pdf",
+  },
+  {
+    name: "Carbon Leakage and competitiveness in the EU until 2050",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D3.3b_Second_Policy_Brief.pdf",
+  },
+  {
+    name: "Focus Report on behavioural effects and distributional impacts",
+    link: "https://zenodo.org/record/3368507#.XX9COJNKhTE",
+  },
+  {
+    name: "Policy Brief – The Role of Behaviour and Heterogeneity for the Adoption of Technologies",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/REEEM-D4.3b.pdf",
+  },
+  {
+    name: "Focus Report on climate impacts on the Energy-Food-Water nexus",
+    link: "https://zenodo.org/record/3368524#.XX9ClJNKhTE",
+  },
+  {
+    name: "Focus Report on LCA and critical material demand for energy technologies",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D5.3-LCA-and-Demand-for-Critical-Materials.pdf",
+  },
+  {
+    name: "Policy Brief",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D5.5b_Policy-Briefs.pdf",
+  },
+  {
+    name: "Technology Roadmaps",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D2.1c.-REEEM-Innovation-and-Technology-Roadmap_GA.pdf",
+  },
+  {
+    name: "Innovation Readiness Level assessments",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D2.2.c-REEEM-IRL-report-Energy-Efficiency-Final.pdf",
+  },
+  {
+    name: "Stakeholder Interaction Portal",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/REEEM-D7.1_D7.2_july2019.pdf",
+  },
+  {
+    name: "Pathways Diagnostic Tool",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/REEEM-D7.1_D7.2_july2019.pdf",
+  },
+  {
+    name: "Open-source Engagement Model",
+    link: "http://www.reeem.org/wp-content/uploads/2019/07/D7.3b_OSeMBE.pdf",
+  },
+  {
+    name: "Online Energy Systems Learning Simulation",
+    link: "https://zenodo.org/record/3368576#.XWOrNJNKhTE",
+  },
+]
+
+
+
+
+
+
+
--- src/components/leftArea.js
+++ src/components/leftArea.js
@@ -1,26 +1,40 @@
-import PropTypes from "prop-types"
 import styled from "styled-components"
 import React from "react"
+import latestDeliverables from "./latestDeliverables"
 
-const LeftArea = ({ disclaimer }) => (
-  <DisclaimerStyle>{disclaimer}</DisclaimerStyle>
+const LeftArea = () => (
+  <LeftAreaContainer>
+    <TopLeftArea>FOLLOW:</TopLeftArea>
+    <MainLeftArea>
+      <h1>LATEST DELIVERABLES</h1>
+      {latestDeliverables.map(deliverable => (
+        <p>
+          <a href={deliverable.link} id={deliverable.name}>
+            {deliverable.name}
+          </a>
+        </p>
+      ))}
+    </MainLeftArea>
+  </LeftAreaContainer>
 )
 
-LeftArea.propTypes = {
-  disclaimer: PropTypes.string,
-}
-
-LeftArea.defaultProps = {
-  disclaimer: `FOLLOW`,
-}
-
-const DisclaimerStyle = styled.div`
+const LeftAreaContainer = styled.div`
   flex: 0 1 340px;
   color: #777;
   font-size: 15px;
   background: white;
   margin: 0px;
   line-height: 24px;
+`
+
+const TopLeftArea = styled.div`
+  color: white;
+  background: #3b8dbd;
+`
+
+const MainLeftArea = styled.div`
+  color: #777;
+  background: #f0f0f0;
 `
 
 export default LeftArea
