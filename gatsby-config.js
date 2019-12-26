c6be9192b972d28a63c6265c4dd038cb98bcc596
olavurellefsen <oe@tokni.com>
2019-12-26 00:25:56 +0000

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `REEEM`,
     description: `Energy Systems Modelling Project`,
-    author: `@gatsbyjs`,
+    author: `@olavur`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/reeem-r.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
