89890c7df4433366b22889b587d1ecfb7d793ae5
olavurellefsen <oe@tokni.com>
2019-12-31 18:07:03 +0000

    Add text and images for EMP-E 2017 event page

--- src/pages/event/european-modelling-platform-for-europe-emp-e-2017-in-brussels.js
+++ src/pages/event/european-modelling-platform-for-europe-emp-e-2017-in-brussels.js
@@ -1,12 +1,296 @@
 import React from "react"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import kthLogo from "../../images/logo/kth.jpg"
+import rliLogo from "../../images/logo/rli.png"
+import empe from "../../images/empe_150x150.png"
+import eu from "../../images/eu_320x320.png"
+import reeem from "../../images/reeem-logo.png"
 
-export default (props) => (
-  <Layout pageTitle="EMP-E Event Page" location={props.location} showSocialMedia={['top', 'bottom']}>
+export default props => (
+  <Layout
+    pageTitle="EMP-E Event Page"
+    location={props.location}
+    showSocialMedia={["bottom"]}
+  >
     <SEO title="EMP-E Event Page " />
-    <p>
-Lorum ipsum
-    </p>
+    <h1>
+      Energy Modelling Platform for Europe (EMP-E) 2017 in Brussels
+    </h1>
+    <div>
+      <h2>
+        17th May 2017 - 18th May 2017
+      </h2>
+    </div>
+    <div>
+      <div>
+        <h3>
+          <img
+            src={empe}
+            alt=""
+            width="150"
+            height="150"
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <img
+            src={eu}
+            alt=""
+            width="150"
+            height="150"
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;
+          <img
+            src={reeem}
+            alt=""
+            width="300"
+            height="83"
+          />
+        </h3>
+        <h3>Subject</h3>
+        <p>
+          The objective of this first <strong>EMP-E</strong> meeting is to
+          provide a peer-reviewed digest of model and policy insights for
+          European energy scenario projects. It aims to initiate a long-term
+          forum for exchanging research, development and practice of energy
+          system modelling in Europe and, where feasible, promote the sharing of
+          data and resources and improve the efficiency of research in the area.
+          The results of the meeting will be published in a special issue of the
+          journal “Energy Strategy Reviews”, which will be provided as input to
+          high-level events such as COP23 [1] in Bonn.
+        </p>
+        <p>
+          Furthermore, as a first meeting it provides the possibility to discuss
+          future <strong>EMP-E</strong> meeting structure and format with the
+          aim to propose follow-up meetings. The <strong>EMP-E</strong> fulfils
+          a recommendation of the Horizon 2020 Advisory Group [2] for Energy to
+          set up a European forum for energy modelling [3].
+        </p>
+        <p>
+          Key themes to be highlighted in the 2017 meeting are the ‘energy
+          union’, its ‘transition’, transparency, scenario definitions and the
+          SET Plan.&nbsp;Furthermore the <strong>EMP-E</strong> concept will be
+          discussed with the aim to propose follow-up actions.
+        </p>
+        <h3>Agenda / Programme</h3>
+        <p>
+          You find the{" "}
+          <strong>
+            <a href="http://www.reeem.org/wp-content/uploads/2017/05/Abstracts_Speeches.pdf">
+              abstracts of the speeches here
+            </a>
+          </strong>
+          .
+        </p>
+        <p>
+          Current programme can be{" "}
+          <strong>
+            <a href="http://www.reeem.org/wp-content/uploads/2017/05/Programme_v03.pdf">
+              downloaded here
+            </a>
+          </strong>{" "}
+          (v3).
+        </p>
+        <p>
+          Presentations and slides can be{" "}
+          <a href="http://www.reeem.org/index.php/emp-e-main/">accessed here</a>
+          .
+        </p>
+        <h4>Focus Groups</h4>
+        <ul>
+          <li>
+            <strong>
+              <a href="http://www.reeem.org/wp-content/uploads/2017/04/EMP_E-Focus_Group_Pathways.pdf">
+                Designing and comparing energy transition pathways
+              </a>
+              <br />
+            </strong>
+            KTH and IER (contact: gardumi@kth.se (Francesco Gardumi))
+          </li>
+          <li>
+            <strong>
+              <a href="http://www.reeem.org/wp-content/uploads/2017/05/EMP-E_Focus-Group_Capacity-expansion-in-high-RES-worlds.pdf">
+                Capacity expansion planning in high RES worlds
+              </a>
+              <br />
+            </strong>
+            Artelys and KIT&nbsp; (contact: laurent.fournie@artelys.com (Laurent
+            Fournie))
+          </li>
+          <li>
+            <strong>
+              <a href="http://www.reeem.org/wp-content/uploads/2017/11/EMP-E-Focus-Group-3-Publishing-in-open-databases.pdf">
+                Publishing in open databases and increasing transparency and
+                reproducibility
+              </a>
+              <br />
+            </strong>
+            RLI (contact: berit.mueller@rl-institut.de (Berit Müller))
+          </li>
+          <li>
+            <strong>
+              <a href="http://www.reeem.org/wp-content/uploads/2017/05/EMP-E_Focus-Group_Bridging-the-gap-between-policy-questions-and-models.pdf">
+                Bridging the gap between policy questions and models
+              </a>
+              <br />
+            </strong>
+            TNO (contact: Christian.Bos@tno.nl (Christian Bos))
+          </li>
+          <li>
+            <strong>
+              <a href="http://www.reeem.org/wp-content/uploads/2017/04/EMP-E_Focus-Group_Modelling-of-environmental-impacts-and-externalities.pdf">
+                Modelling of environmental impacts and externalities
+              </a>
+              <br />
+            </strong>
+            DTU&nbsp; (contact: keka@dtu.dk (Kenneth Karlson))
+          </li>
+          <li>
+            <strong>
+              <a href="http://www.reeem.org/wp-content/uploads/2017/05/EMP-E_Focus-Group06_Linking-models_V03.pdf">
+                Methodologies for linking models
+              </a>
+              <br />
+            </strong>
+            IER and TU Wien (contact: ulrich.fahl@ier.uni-stuttgart.de)
+          </li>
+        </ul>
+        <h3>Call for papers</h3>
+        <p>
+          <strong>
+            <a href="http://www.reeem.org/wp-content/uploads/2017/06/ESR_Call-for-papers_02.pdf">
+              Link to the call for papers
+            </a>{" "}
+            (v2)
+            <br />
+          </strong>
+        </p>
+        <h3>Deadlines</h3>
+        <ul>
+          <li>
+            Registration
+            Deadline
+            7th April 2017
+          </li>
+          <li>
+            EMP-E
+            conference
+            17th/18th&nbsp;May 2017
+          </li>
+          <li>
+            Submission of journal paper (4 pages or 6-10
+            pages)
+            <strong>
+              15th July 2017
+              <br />
+            </strong>
+          </li>
+        </ul>
+        <h3>Time and place</h3>
+        <p>
+          The conference will take place on{" "}
+          <strong>17-18th of May, 2017</strong>
+          <br />
+          at the <strong>RTD/JRC headquarters</strong> in Brussels.
+          <br />
+          <a href="http://www.openstreetmap.org/node/2449698114#map=16/50.8382/4.3685">
+            21 rue du champ de Mars / Marsveldstraat 21, 1050 Brussels
+          </a>
+        </p>
+        <h3>Registration</h3>
+        <p>Registration is only possible with invitation.</p>
+        <h3>Organisation committee</h3>
+        <p>
+          <img
+            src={rliLogo}
+            alt=""
+            width="75"
+            height="56"
+          />
+          &nbsp;&nbsp;{" "}
+          <img
+            src={kthLogo}
+            alt=""
+            width="54"
+            height="54"
+            sizes="(max-width: 54px) 100vw, 54px"
+          />
+          <br />
+          European Commission DG RTD
+        </p>
+        [1] Conference of the Parties (COP 23) to the UN Convention on Climate
+        Change (UNFCCC){" "}
+        <a href="http://sdg.iisd.org/events/unfccc-cop-23/">
+          http://sdg.iisd.org/events/unfccc-cop-23/
+        </a>
+        <br />
+        [2]{" "}
+        <a href="http://ec.europa.eu/transparency/regexpert/index.cfm?do=groupDetail.groupDetail&amp;groupID=2981">
+          http://ec.europa.eu/transparency/regexpert/index.cfm?do=groupDetail.groupDetail&amp;groupID=2981
+        </a>
+        <br />
+        [3] Recommendation 9.d. Page&nbsp;14 :&nbsp;
+        <a href="http://ec.europa.eu/transparency/regexpert/index.cfm?do=groupDetail.groupDetailDoc&amp;id=25609&amp;no=1">
+          http://ec.europa.eu/transparency/regexpert/index.cfm?do=groupDetail.groupDetailDoc&amp;id=25609&amp;no=1
+        </a>
+      </div>
+      <div>
+        <div><br/><br/>
+          <h2> Details </h2>
+          <dl>
+            <dt> Start: </dt>
+            <dd>
+              <abbr
+                title="2017-05-17"
+              >
+                {" "}
+                17th May 2017{" "}
+              </abbr>
+            </dd>
+            <dt > End: </dt>
+            <dd>
+              <abbr
+                title="2017-05-18"
+              >
+                {" "}
+                18th May 2017{" "}
+              </abbr>
+            </dd>
+            <dt>
+              Event Category:
+            </dt>{" "}
+            <dd>
+              <a
+                href="http://www.reeem.org/index.php/events/category/conference/"
+                rel="tag"
+              >
+                Conference
+              </a>
+            </dd>
+            <dt>Event Tags:</dt>
+            <dd>
+              <a href="http://www.reeem.org/index.php/tag/emp-e/" rel="tag">
+                EMP-E
+              </a>
+            </dd>
+          </dl>
+        </div>
+
+        <div>
+          <h2>Organiser</h2>
+          <dl>
+            <dt></dt>
+            <dd>Berit Müller </dd>
+            <dt>Email: </dt>
+            <dd>berit.mueller@rl-institut.de </dd>
+            <dt>Website: </dt>
+            <dd>
+              <a href="http://reiner-lemoine-institut.de/">
+                http://reiner-lemoine-institut.de/
+              </a>
+            </dd>
+          </dl>
+        </div>
+      </div>
+    </div>
   </Layout>
 )
