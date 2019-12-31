2d6114e2bb1e68d3d9a91b3ac477d8007b4af539
olavurellefsen <oe@tokni.com>
2019-12-31 17:35:45 +0000

    Add EMP-E 2017 poster links

--- src/pages/emp-e-poster.js
+++ src/pages/emp-e-poster.js
@@ -2,11 +2,455 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default (props) => (
-  <Layout pageTitle="EMP-E Posters" location={props.location} showSocialMedia={['top', 'bottom']}>
+export default props => (
+  <Layout
+    pageTitle="EMP-E Posters"
+    location={props.location}
+    showSocialMedia={["top", "bottom"]}
+  >
     <SEO title="EMP-E Posters" />
-    <p>
-Lorum ipsum
-    </p>
+    <div>
+      <h2>EMP-E Poster &amp; Networking Space</h2>
+      <p>
+        <span id="result_box" class="" lang="en">
+          <span class="">
+            The copyright for all posters lies with the respective authors.
+          </span>{" "}
+          <span class="">
+            We have the permission to publish all on this website. For further
+            use and distribution of the content please view the specific
+            copyright notes (if available) or contact the authors!
+          </span>
+        </span>
+      </p>
+      <h4>Different Dimensions of Energy Modelling</h4>
+      <table width="636">
+        <tbody>
+          <tr>
+            <td width="48">DD.1</td>
+            <td width="150">Ruud Egging (NTNU)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/taEoyGxLV4CKQm5"
+              >
+                SET-Nav – case studies on Europe’s strategic energy technologies
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">DD.2</td>
+            <td width="150">Gustav Resch (TU Wien)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/29pz9zpLsqJsb8f"
+              >
+                SET-Nav – The project at a glance
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">DD.3</td>
+            <td width="150">Angelo Martino (TRT)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/ijCsDW6bnFGba4R"
+              >
+                Reflex – Analysis of the European Energy System under the
+                Aspects of Flexibility and Technological Progress
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">DD.4</td>
+            <td width="150">Linas Martišauskas (LEI)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/gSg6Nx4ZKpNkSGS"
+              >
+                Methodology for energy security assessment considering energy
+                system resistance to disruptions
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">DD.5</td>
+            <td width="150">Arash Shojachaikar (ZIRIUS)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/AwJxxFmjSnCQgFI"
+              >
+                Linking social science and energy modeling: Inclusion of social
+                system analysis in modeling practices
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">DD.6</td>
+            <td width="150">Behnam Zakeri (Aalto)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/vE6xAMQRoCRHQDQ"
+              >
+                Interconnection of the UK to the Nordic power market – Impacts
+                on social welfare and renewable integration
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>Flexibilities/Storage</h4>
+      <table width="636">
+        <tbody>
+          <tr>
+            <td width="48">F.1</td>
+            <td width="150">Arjuna Nebel (WI)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/auPtKYC91YQELlY"
+              >
+                Modeling the interdependencies of storage, dsm and grid
+                extension for europe
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">F.2</td>
+            <td width="150">Frank Meinke-Hubeny (Vito)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/Xg4irfPkHUkmNBo"
+              >
+                ESTMAP – Energy Storage Mapping and Planning, Energy System
+                analysis (2 roll-ups)
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>Models</h4>
+      <table width="636">
+        <tbody>
+          <tr>
+            <td width="48">M.1</td>
+            <td width="150">Ndaona Chokani (LEC)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/pU0uJyeSjzEmv8g"
+              >
+                EnerPol: High-Resolution Integrated Framework for Analysis of
+                Electricity, Gas, Transport and Urban Systems
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">M.2</td>
+            <td width="150">Jody Dillon (UCD)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/9jlcYi0wpbwLnw5"
+              >
+                Epiphron – A flexible tool for modelling high VG systems across
+                mutliple time scales
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">M.3</td>
+            <td width="150">Jonas Hörsch (FIAS)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/biAeFjRXhcykp5e"
+              >
+                PyPSA-EU-Grid: An Open Optimization Model of the European
+                Transmission System
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">M.4</td>
+            <td width="150">
+              Valentin Bertsch, Muireann Lynch, Mel Devine (ESRI)
+            </td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/6gOZQjBBwVE4zML"
+              >
+                Models for Quantifying Benefits of Integrated Energy Systems
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">M.5</td>
+            <td width="150">Vidas Lekavicius (LEI)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/oDvP9PDq1qswxZZ"
+              >
+                Linking CGE and Energy Planning Models – a Case Study for
+                Lithuania
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">M.6</td>
+            <td width="150">Pinar Korkomaz (IER)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/DVJ4rrCDJu8Pgdu"
+              >
+                Model based analysis of transformation pathways to a sustainable
+                European energy system
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">M.7</td>
+            <td width="150">Alexis Laurent and Serena Fabbri (DTU)</td>
+            <td width="438">
+              Assessing environmental impacts of future energy systems: a
+              holistic LCA model for Europe in 2015-2050
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">M.8</td>
+            <td width="150">Wided Medroubi (Next Energy)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/gx3e3Un0wh6NKZp"
+              >
+                SciGRID: a first experience with creating and publishing open
+                data and models of the European power transmission network
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">M.9</td>
+            <td width="150">
+              Berit Müller
+              <br />
+              (RLI)
+            </td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/CfXc4qj2637ZAUx"
+              >
+                open energy modelling framework (oemof)
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">M.10</td>
+            <td width="150">Roland Montenegro (IER)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/If4Pcw90dgruva8"
+              >
+                Multi-sectoral modelling of decarbonisation pathways: a pilot
+                thought experiment
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">M.11</td>
+            <td width="150">Dogan Keles (KIT)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/UYbCTI4SdcGgod1"
+              >
+                PERSEUS-EU – Generation and expansion planning optimization
+                model for the European energy system
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>Transparency in European energy system analyses</h4>
+      <table width="636">
+        <tbody>
+          <tr class="alt">
+            <td width="48">T.1</td>
+            <td width="150">Ludwig Hülk (RLI)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/EGm7vHut8rKvdNC"
+              >
+                The OpenEnergy Database (oedb) – A database concept to improve
+                transparency in energy system analyses
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">T.2</td>
+            <td width="150">Georgios Avgerinopoulos (KTH)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/sU4axWJaJPjl78o"
+              >
+                Energydata.info – An innovation of the World Bank Group
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">T.3</td>
+            <td width="150">Robbie Morrison (openmod)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/G3pVZVcOiTVHYlJ"
+              >
+                Improving energy sysetem modelling – Open Energy Modelling
+                Initiative
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">T.4</td>
+            <td width="150">Pierre Iachetti (EURAC)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/UY56lYiyjXqAjKN"
+              >
+                Hotmaps – The open source mapping and planning tool for heating
+                and cooling
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>Environmental impacts of the energy sector</h4>
+      <table width="636">
+        <tbody>
+          <tr class="alt">
+            <td width="48">E.1</td>
+            <td width="150">Ulla Mörtberg (KTH)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/cIQHTbng5Aecdcq"
+              >
+                The Landscape simulation and Ecological Assessment (LEcA) tool –
+                A case study of Lithuania
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">E.2</td>
+            <td width="150">
+              Dorothea Schmid
+              <br />
+              (IER)
+            </td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/VKjqOtMXa2LzF2H"
+              >
+                Analysis of impacts of different emission reduction scenarios on
+                emission unit prices in Europe
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>Pathways</h4>
+      <table width="636">
+        <tbody>
+          <tr class="alt">
+            <td width="48">P.2</td>
+            <td width="150">Laura Gutierrez (IRENA)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/42OfisLjUEvnZhP"
+              >
+                IRENA’s work for a Global Renewable Energy Future: 1) Key
+                programmes and activities; 2) REmap (Renewable Energy Roadmap)
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">P.3</td>
+            <td width="150">Christian Bussar (RWTH Aachen)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/sy91RNoCAAZspBM"
+              >
+                GENESYS 2: Optimisation of Pathways Towards a Low-Carbon
+                European Energy System
+              </a>
+            </td>
+          </tr>
+          <tr class="alt">
+            <td width="48">P.4</td>
+            <td width="150">
+              Antoine Levesque
+              <br />
+              (PIK)
+            </td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/iIDyV3kJ69zYU01"
+              >
+                Improving REMIND to create more detailed EU decarbonization
+                scenarios for the INNOPATHS project
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">P.5</td>
+            <td width="150">Heidi Ursula Heinrichs (FZJ)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/VyvopkqkyNalgP5"
+              >
+                Towards transparency – the issue of socio-economic scenario
+                frameworks
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <h4>Modelling at different scales</h4>
+      <table width="636">
+        <tbody>
+          <tr class="alt">
+            <td width="48">S.1</td>
+            <td width="150">
+              Sofia Simoes
+              <br />
+              (UNL)
+            </td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/Kdt4nViDTf0agHy"
+              >
+                Integrative Smart City Planning – Urban energy system modelling
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td width="48">S.2</td>
+            <td width="150">Pia Manz (ISI)</td>
+            <td width="438">
+              <a
+                href="https://cloud.rl-institut.de/index.php/s/8cK5vZtGfbSRj7E"
+              >
+                Modelling electricity demand with high spatial and temporal
+                resolution
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <p>
+        <span id="result_box" class="" lang="en">
+          <span class="">
+            The copyright for all posters lies with the respective authors.
+          </span>{" "}
+          <span class="">
+            We have the permission to publish all on this website. For further
+            use and distribution of the content please view the specific
+            copyright notes (if available) or contact the authors!
+            <br />
+          </span>
+        </span>
+      </p>
+    </div>
   </Layout>
 )
