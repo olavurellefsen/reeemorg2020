5995126d647b362604a979e98a60b022fafb2979
olavurellefsen <oe@tokni.com>
2019-12-31 14:03:31 +0000

    Add EMP-E 2018 and 2019 texts and logo

--- src/pages/emp-e-2018.js
+++ src/pages/emp-e-2018.js
@@ -2,11 +2,194 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default (props) => (
-  <Layout pageTitle="EMP-E 2018" location={props.location} showSocialMedia={['top', 'bottom']}>
+export default props => (
+  <Layout
+    pageTitle="EMP-E 2018"
+    location={props.location}
+    showSocialMedia={["top", "bottom"]}
+  >
     <SEO title="EMP-E 2018" />
     <p>
-Lorum ipsum
+      The Energy Modelling Platform for Europe (EMP-E) was first created within
+      the REEEM project. Its first event was held in Brussels on May 17th-18th,
+      2017;
+    </p>
+    <p>
+      The second meeting of the{" "}
+      <a href="https://www.energymodellingplatform.eu/">EMP-E</a> was on
+      September 25th-26th 2018 in Brussels, at the premises of the European
+      Commission’s DG R&I.
+    </p>
+    <p>
+      Key themes discussed at the 2018 meeting were Innovation in the energy
+      transition, Integrated modelling of energy and resource efficiency and
+      Modelling of behavioural aspects. The first day was dedicated to plenary
+      sessions, while the second day was dedicated to parallel focus group
+      sessions. On both days the participants were provided space and time for
+      poster presentations on their modelling activities, research and insights.
+      Linked to the event, a call for papers for a Special Issue of Energy
+      Strategy Reviews (Elsevier) was issued, with deadline for full paper
+      submission on September 12th 2018.
+    </p>
+    <p>
+      REEEM partners were in the{" "}
+      <a href="http://www.energymodellingplatform.eu/scientific-committee.html">
+        Scientific Committee
+      </a>{" "}
+      of the 2018 event, too.
+    </p>
+    <p>
+      REEEM organised the plenary session on ‘Integrated modelling of energy and
+      resource efficiency’, held on September 25th 2018 and moderated by Dr.
+      Kenneth Karlsson (DTU).
+    </p>
+    <p>
+      REEEM partners also organised the following parallel sessions, held on
+      September 26th:
+    </p>
+    <h4>1A - open databases</h4>
+    <p>
+      In this focus group we bring together participants from various projects
+      who aim for or even manage to openly provide their data. We will discuss
+      how we can improve the exchange or linking of data that we use in energy
+      system analyses and what is needed to facilitate that willing projects can
+      easier open up their data.
+    </p>
+    <p>
+      With this focus group we aim at defining tasks to reach a higher share of
+      open data and a better cooperation between the EU-projects concerning data
+      that all of them normally use.
+    </p>
+    <p>
+      The session will start with short inputs about data handling in the
+      projects present at the focus group (how do you organize/structure your
+      data and how do you provide it to partners and eventually other
+      stakeholders? in your opinion, what is the most important ToDo to improve
+      exchange of data between EU-projects?). For the following discussion we
+      will devide into handable working groups and tackle the topics:
+    </p>
+    How openly provide and bring together our data (common DBs or links,
+    datapackages or…?) Which steps to take to really do it? Do we need to
+    harmonize meta data and the categorisation of data? If yes, how can we
+    achieve that? Which data is often needed for analyses and not available
+    (e.g. costs, distribution lines, industry energy demand, behaviour,…) in
+    which ways do you help yourself to replace it/ to generate the data
+    synthetically… How can we share the scarce information available?
+    <p>
+      If you are interested to participate in this group and to explain how your
+      project handles the modelling data we are happy to receive a short note as
+      soon as possible to better prepare the discussions.
+    </p>
+    <p>Projects that already confirmed their participation:</p>
+    <p>
+      <a href="https://www.reeem.org/">REEEM</a>,{" "}
+      <a href="http://medeas.eu/#home">MEDEAS</a>,{" "}
+      <a href="http://www.set-nav.eu/">SET-Nav</a>,{" "}
+      <a href="http://www.european-calculator.eu/">EU-CALC</a>,{" "}
+      <a href="http://reflex-project.eu/">REFLEX</a>
+    </p>
+    <p>
+      Contact information: Kenneth Karlsson{" "}
+      <a href="mailto:kek@dtu.dk">kek@dtu.dk</a>
+    </p>
+    <h4>1B - energy, climate and macroeconomic relationships</h4>​
+    <p>
+      The struggle of the EU to move towards a carbon free society results in an
+      increasing concern about human induced climate change. Consequently, the
+      environmental, economic and energetic impacts of policies are considered
+      of equal importance. Therefore, this session aims on identifying different
+      modelling frameworks that address each of these aspects (economy, energy
+      and environment) and on discussing how they can be jointly applied in
+      order to deliver an integrated assessment of policies.
+    </p>
+    <p>
+      The session consists of two parts. The first part will mainly show the
+      current state of research in this area. Three experts will outline the
+      objectives of integrated analyses and present their respective coupling
+      approaches. Furthermore, they will specify which aspects of the three
+      dimensions they have taken into account in their models. Based on their
+      current research experience they will give insights on occurring
+      interactions and, subsequently, point out the requirements needed for
+      further development of model linking approaches. In addition, the
+      consideration of innovation, behaviour and environmental externalities
+      will be addressed within this section. Hence, each speaker will give a
+      short overview on how these aspects can be implemented. In the second part
+      all three presenters will be invited to participate in a moderated panel
+      discussion.
+    </p>
+    <p>
+      Contact information: Mohammad Ahanchian{" "}
+      <a href="mailto:Mohammad.Ahanchian@ier.uni-stuttgart.de">
+        Mohammad.Ahanchian@ier.uni-stuttgart.de
+      </a>
+    </p>
+    <h4>2B - modelling of behavioural aspects</h4>
+    <p>
+      Format: The session will begin with scene-setting input from the European
+      Commission followed by presentations from the panelists. Following these,
+      and after a brief Q&A with the audience, we will begin a discussion with
+      the panelists and the audience to address the questions outlined in the
+      objectives above. Participants will also be provided with a scorecard to
+      document their responses to the workshop questions.
+    </p>
+    <p>
+      Objective: In recent years there has been increasing interest and need to
+      understand and incorporate behavioral aspects into energy modelling. The
+      interdisciplinary nature of energy behavior means a common language and
+      platform for exchange on the topic is required. While there are numerous
+      models available, each incorporates different aspects and sectors of
+      behavior with varying methodologies. Given the sometimes contrasting focus
+      from specific disciplines, it would be useful to explore how behavioral
+      aspects are included with the aim of providing energy system-wide policy
+      recommendations. The plenary session on “Modelling of behavioral aspects”
+      looked at the different modelling approaches to explore how behavior is
+      included into different energy models and why these approaches would be
+      appropriate depending on the policy questions at hand. In this focus
+      group, we will explore deeper not only the models and their methodologies,
+      but also the specific behavioural aspects to be included in energy
+      modelling. With this in mind, the focus will be to discuss the following
+      questions:
+    </p>
+    In which sectors and how is behavior included (buildings, transport,
+    conversion, cross-cutting?) What are the main barriers in the building and
+    transport sectors related to behavioral aspects? How are the perspectives on
+    behavior from different disciplines integrated into energy modelling?
+    <p>
+      Contact information: Mohammad Ahanchian{" "}
+      <a href="mailto:Mohammad.Ahanchian@ier.uni-stuttgart.de">
+        Mohammad.Ahanchian@ier.uni-stuttgart.de
+      </a>
+    </p>
+    <h4>3B - regional vs national vs Europe-wide modelling</h4>
+    <p>
+      What do policy makers need when it comes to making national energy and
+      climate policy decisions.
+    </p>
+    <p>
+      High level energy and climate target are negotiated at EU level and
+      implemented at national level by national governments. As part of new EU
+      governance rules, Member States must develop short and long term
+      Integrated National Energy and Climate Plans. These plans will be informed
+      by modelling.
+    </p>
+    <p>
+      Understand the policy making process is integral to achieving impact with
+      modelling results. This focus group looks at our community from a policy
+      maker perspective and discusses the requirements (both technical and
+      non-technical) of policy makers when using model results and asks how
+      academic research can create relevant policy impact. Two policy makers
+      from national government to give their perspectives and answer questions
+      from listeners.
+    </p>
+    <p>
+      Contact information: Deane, Paul{" "}
+      <a href="mailto:jp.deane@ucc.ie">jp.deane@ucc.ie</a>
+    </p>
+    <p>
+      <a href="http://www.energymodellingplatform.eu/program-and-presentations.html">
+        See the full EMP-E 2018 program and presentations
+      </a>
+      .
     </p>
   </Layout>
 )
--- src/pages/emp-e-2019.js
+++ src/pages/emp-e-2019.js
@@ -1,12 +1,91 @@
 import React from "react"
+import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import empe from "../images/empe_720x360.jpg"
 
-export default (props) => (
-  <Layout pageTitle="EMP-E 2019" location={props.location} showSocialMedia={['top', 'bottom']}>
+export default props => (
+  <Layout
+    pageTitle="EMP-E 2019"
+    location={props.location}
+    showSocialMedia={["top", "bottom"]}
+  >
     <SEO title="EMP-E 2019" />
     <p>
-Lorum ipsum
+      The first Energy Modelling Platform for Europe (EMP-E) event was created
+      within the REEEM project and was held in Brussels in 2017.
+    </p>
+
+    <p>
+      The second EMP-E was held held on 25th-26th September 2018, at the
+      premises of the European Commission’s DG R&I in Brussels.
+    </p>
+
+    <ImgStyle src={empe} alt="Energy Modelling Platform for Europe logo" />
+    <p>
+      The third <a href="http://www.energymodellingplatform.eu/home-emp-e-2019.html">Energy Modelling Platform for Europe Conference (EMP-E 2019)</a>,
+      hosted by the European Commission, will take place on 8th – 9th October 2019 in
+      Brussels.
+    </p>
+    <p>
+      This year’s conference is hosted by nine projects funded within the{" "}
+      <a href="https://ec.europa.eu/programmes/horizon2020/en">
+        European Union’s Horizon 2020 Research and Innovation programme
+      </a>{" "}
+      – <a href="https://www.medeas.eu/">MEDEAS</a>,
+      <a href="https://magic-nexus.eu/">MAGIC</a>,{" "}
+      <a href="https://www.magnitude-project.eu/">MAGNITUDE</a>,{" "}
+      <a href="https://www.plan4res.eu/project/the-project/">plan4res</a>,{" "}
+      <a href="https://www.h2020-planet.eu/">PLANET</a>, REEEM,{" "}
+      <a href="http://reflex-project.eu/">Reflex</a>,{" "}
+      <a href="http://www.set-nav.eu/">SET-Nav</a> and{" "}
+      <a href="http://www.spine-model.org/">Spine</a> – and is open to experts,
+      scientists, industry, civil society and policy makers, to exchange views
+      and research on current energy modelling issues.
+    </p>
+    <p>
+      A <b>networking space</b>, where all participants can meet and exchange on
+      common questions and the exhibited posters, constitutes a central part of
+      the conference and provides an overview of the EU model-landscape and an
+      insight to ongoing projects.
+    </p>
+    <p>
+      In four plenary sessions, these key themes will be discussed on day 1:
+    </p>
+    <ol type="I">
+      <li>
+        Achieving ambitious RES deployment in a multi-coupled European energy
+        system – technoeconomical, financial and societal challenges
+      </li>
+      <li>The effects of externalisation on decarbonisation</li>
+      <li>Pathways and scenarios towards the Paris agreement</li>
+      <li>
+        Decarbonisation of cities: Modelling energy transition, sector coupling
+        and cross-sectoral challenges
+      </li>
+    </ol>
+    <p>
+      REEEM co-organises with <a href="http://www.set-nav.eu/">SET-Nav</a> the plenary session lll “Pathways and
+      scenarios towards the Paris agreement”.
+    </p>
+    <p>
+      More detailed discussions and active work will be possible on day 2 in
+      several <b>focus groups</b> with specialised <b>topics</b> where proactive
+      contribution of participants is welcome.
+    </p>
+    <p>
+      The results of the meeting will be published after a peer-review process
+      in a <b>Special Issue</b> of the Journal Energy Strategy Reviews
+      (Elsevier).
+    </p>
+    <p>
+      <a href="http://www.energymodellingplatform.eu/program-and-presentations.html">
+        See the full EMP-E 2019 program.
+      </a>
     </p>
   </Layout>
 )
+
+const ImgStyle = styled.img`
+  margin-top: 20px;
+`
