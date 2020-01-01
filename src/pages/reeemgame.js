3316de7b602dd71ed17980bceaa9d312ff513010
olavurellefsen <oe@tokni.com>
2020-01-01 10:56:14 +0000

    Add reeemgame and reeempathways links

--- src/pages/reeemgame.js
+++ src/pages/reeemgame.js
@@ -3,10 +3,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default (props) => (
-  <Layout pageTitle="REEEMgame" location={props.location} showSocialMedia={['top', 'bottom']}>
+  <Layout pageTitle="REEEMgame" location={props.location} showSocialMedia={['bottom']}>
     <SEO title="REEEMgame" />
     <p>
-Lorum ipsum
+      The REEEMgame is available at <a href="https://reeemgame.org">reeemgame.org</a>
     </p>
   </Layout>
 )
--- src/pages/reeempathways.js
+++ src/pages/reeempathways.js
@@ -2,11 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default (props) => (
-  <Layout pageTitle="REEEMpathways tool" location={props.location} showSocialMedia={['top', 'bottom']}>
+export default props => (
+  <Layout
+    pageTitle="REEEMpathways tool"
+    location={props.location}
+    showSocialMedia={["bottom"]}
+  >
     <SEO title="REEEMpathways tool" />
     <p>
-Lorum ipsum
+      The REEEMpathways tool is available at{" "}
+      <a href="https://reeempathways.org">reeempathways.org</a>
     </p>
   </Layout>
 )
