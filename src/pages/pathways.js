6fae0c78cbca9596515663cd3f8094f061009367
olavurellefsen <oe@tokni.com>
2020-01-01 09:18:15 +0000

    Add pathways text and table image

--- src/pages/pathways.js
+++ src/pages/pathways.js
@@ -1,12 +1,217 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import pathwaySummaryTable from "../images/pathway-summary-table.png"
 
-export default (props) => (
-  <Layout pageTitle="Pathways" location={props.location} showSocialMedia={['top', 'bottom']}>
+export default props => (
+  <Layout
+    pageTitle="Pathways"
+    location={props.location}
+    showSocialMedia={["top", "bottom"]}
+  >
     <SEO title="Pathways" />
-    <p>
-Lorum ipsum
-    </p>
+    <div class="entry themeform">
+      <h2>
+        Narratives and main assumptions of the three EU Transition Pathways
+        formulated in REEEM:
+      </h2>
+      <p>
+        <strong>Coalitions for a low-carbon path:</strong>
+        <em>
+          energy carrier suppliers take on the highest burden in the
+          decarbonisation of the EU energy system, with consumers observing this
+          transition in mostly a passive way and being reactive to policies as
+          they emerge
+        </em>
+        <i>.</i>
+      </p>
+      <p>
+        <strong>Local solutions:</strong>
+        <em>
+          consumers (especially households) engage more proactively in the
+          transition, through choices on end use appliances, energy efficiency
+          measures and transportation technologies.
+        </em>
+      </p>
+      <p>
+        <strong>Paris Agreement:</strong>
+        <em>
+          the EU undertakes an ambitious decarbonisation effort, with a target
+          of 95% reduction of CO2 emissions by 2050. This overshoots the Paris
+          Agreement pledges. Both energy carrier suppliers and consumers engage
+          in the challenge.
+        </em>
+      </p>
+      <h3>Summary of the pathways</h3>
+      <p>
+        The key characteristics of the storylines underpinning each pathway and
+        the relative main assumptions are summarised in the table below.
+      </p>
+      <p>
+        <img src={pathwaySummaryTable} alt="Pathway summary table" />
+      </p>
+      <h3>Coalitions for a low-carbon path</h3>
+      <p>
+        Economic growth in the EU restarts after the financial crisis, but at
+        different speeds across Member States. Affinity on trade, labour,
+        defence and energy security policy arises between groups of countries,
+        depending on their geographic location, economy and domestic
+        availability of resources. Under a general political recognition of the
+        impacts of climate change, the EU sets itself on a high decarbonisation
+        path. The emission targets for sectors included in the Emission Trading
+        Scheme (ETS) are complied with and overshot in some instances, leading
+        to 83% cut in energy-related CO2 emissions by 2050 compared to 1990. On
+        the contrary, sectors not regulated by the ETS rely more on consumers’
+        choices and reduce emissions to a lesser extent. Additionally, despite
+        the common general ambition to fulfil the Energy Union Strategy and the
+        Paris Agreement, coalitions of more and less willing Member States
+        emerge, setting more and less ambitious decarbonisation targets for
+        non-ETS sectors.&nbsp;
+      </p>
+      <p>
+        A similar coalitional pattern emerges outside Europe, where countries
+        being most affected by climate change extremes and/or having the means
+        take on more climate change mitigation actions than others.&nbsp;
+      </p>
+      <p>
+        Energy carrier suppliers and large industrial users subject to ETS
+        targets take up large part of the decarbonisation effort. On the
+        contrary, even though the effects of climate change are observed in
+        Europe (especially with Southern regions becoming on average warmer and
+        drier), not all consumers across the EU perceive it as likely to affect
+        their lives significantly. Therefore, they tend to hold on to their
+        current consumption behaviours and transition more slowly to energy
+        efficient end-use appliances, unless pushed otherwise by top-down
+        policies. This ultimately causes the levels of ambition for sectors not
+        included in the ETS (such as residential and transportation) to be
+        generally lower and to vary between Member States.&nbsp;
+      </p>
+      <p>
+        Under the pressure of policy targets, utilities undertake large
+        investments in centralised renewable energy supply options. In the
+        electricity sector, solar photovoltaic fields and wind farms (both
+        onshore and offshore) dominate. Innovation in offshore wind by
+        breakthrough of floating platforms contributes to the penetration of the
+        technology in the market. Biomass-fired power plants and, to a minor
+        extent, Carbon Capture and Sequestration (CCS), gain a small share.
+        Decarbonisation measures in end use sectors remain more limited. The
+        rate of renovation of residential buildings increases considerably
+        compared to the current rate, but not enough to guarantee complete
+        renovation of the stock by 2050. Technologies such as heat pumps and
+        solar thermal are not cost competitive enough to gain a share in the
+        energy mix.
+      </p>
+      <p>
+        This pathway was agreed upon as a useful base case for REEEM by the
+        participants of the First Stakeholder Workshop held on October 6th 2017
+        in Brussels. It resembles characteristics of two of the five scenarios
+        described in the ‘
+        <a href="https://ec.europa.eu/commission/sites/beta-political/files/white_paper_on_the_future_of_europe_en.pdf">
+          White paper on the future of Europe
+        </a>
+        ’ discussed by President Jean-Claude Juncker at the State of the Union
+        2017: ‘Carrying on’ and ‘Those who want more do more’.
+      </p>
+      <h3>Local solutions</h3>
+      <p>
+        The transition to a low carbon EU energy system is accelerated by
+        communities and households proactively making choices that are low
+        carbon, such as on household energy provision (i.e., stronger efforts to
+        retrofit homes, purchasing of energy efficient (and smart) appliances,
+        replacement of heating systems).&nbsp;
+      </p>
+      <p>
+        This moderately stronger societal push is underpinned by a{" "}
+        <i>recognition</i> of the increasing and stronger climate signals, both
+        within and outside of Europe (i.e. more pronounced and frequent flooding
+        events in Europe, particularly in the winter, and longer and more
+        extreme hot spells during the summer months, leading to wild fires,
+        water shortages and heat-related health impacts).&nbsp;
+      </p>
+      <p>
+        The pace of change in the demand for low carbon goods initially leaves
+        decision makers in the EU lagging behind in their policy efforts to
+        drive the transition. This lag is particularly evident out to 2030 due
+        to the initial rapid rate of change.
+      </p>
+      <p>
+        Meanwhile, Member States are set on course to 80% reduction in
+        energy-related GHG emissions by 2050 as compared to 1990. However,
+        coalitions of more and less willing countries set different targets for
+        sectors not covered by the Emission Trading Scheme, depending on their
+        geographic location, economy and domestic availability of resources. A
+        coalitional pattern similar to the one within the EU is identifiable
+        outside Europe. These aspects are similar to the Coalitions for a low
+        carbon path.
+      </p>
+      <p>
+        This pathway was suggested by participants at a scenario workshop in
+        Brussels on October 6th 2017 as an interesting scenario narrative to
+        explore in the context of the REEEM project.
+      </p>
+      <h3>Paris Agreement</h3>
+      <p>
+        Due to the IPCC Special Report on Global Warming of 1.5C, the effects of
+        climate change obtain increasing media coverage. Climate strikes start
+        worldwide and put pressure on Governments and public opinion. Awareness
+        of the effects of climate change increases globally, both on the side of
+        Governments and on the one of citizens.&nbsp;
+      </p>
+      <p>
+        In the EU, green parties increase their representation in the
+        Parliament. At the same time, the EU emerges stronger from the Brexit
+        negotiations and the financial crisis. This leads to growing alignment
+        between Member States on environment and climate matters, as envisaged
+        by President Juncker in two scenarios of the ‘White Paper on the future
+        of Europe’. The EU makes a political commitment to taking the world lead
+        in fulfilling the Paris Agreement and sets a target of at least 95%
+        reduction of GHGs emissions by 2050 compared to 1990 levels.&nbsp;
+      </p>
+      <p>
+        On the other side, the increased awareness by citizens results in
+        behavioural shifts and green choices. These come especially when
+        replacing household heating appliances and cars which reached end of
+        life or when offered the chance to renovate old, inefficient buildings.
+      </p>
+      <p>
+        Stringent policies provide the incentives and the policy certainty for
+        large consumers and energy carrier suppliers to also shift towards low
+        carbon options. Industrial consumers replace fossil fuels in their
+        primary energy supply for heating with biomass and electricity. As
+        Carbon Capture and Sequestration technologies become commercial, they
+        are applied to industrial biomass plants, making industry a net negative
+        emissions sector. Energy carrier suppliers also invest significantly in
+        renewable options, particularly for large-scale centralised electricity
+        generation. Overall, increased electrification of the energy supply is
+        observed.
+      </p>
+      <p>
+        The decarbonisation effort undertaken in the EU demonstrates the
+        feasibility of a low-carbon economy and opens market opportunities for
+        other economies, where parts of the value chains of low-carbon
+        technologies are located. In these economies, investments in the
+        production of low-carbon technologies increase quickly and policies are
+        put in place to support these investments. The United States and China
+        take advantage from large economies of scale and solid market shares in
+        the production of low-carbon technologies. This soon gives them a
+        competitive edge and the two economies are set on a path towards
+        decreasing GHGs emissions to Paris Agreement levels. Other world
+        economies follow the same path.&nbsp;
+      </p>
+      <p>
+        In the beginning, the shift from the technologically mature and
+        short-term secure fossil fuel supply and the large infrastructure
+        investments put a burden on European economies. GDP growth across Member
+        States is slowed, hindering the competitiveness of the EU versus China
+        and the United States. However, the rate of growth increases closer to
+        2050. Furthermore, the projected economic benefits from avoided
+        externalities are important.
+      </p>
+      <p>
+        This pathway was first discussed at the REEEM GA in Zagreb 14-15 May
+        2018 as an important future to explore in the context of the REEEM
+        project.
+      </p>
+    </div>
   </Layout>
 )
