ca1052c4d989cae25be540540c0619487d741d01
olavurellefsen <oe@tokni.com>
2020-01-01 10:59:28 +0000

    Add newsletter list

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -2,11 +2,105 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-export default (props) => (
-  <Layout pageTitle="Blog" location={props.location} showSocialMedia={['top', 'bottom']}>
-    <SEO title="Blog" />
-    <p>
-Lorum ipsum
-    </p>
+export default props => (
+  <Layout
+    pageTitle="Newsletter"
+    location={props.location}
+    showSocialMedia={["bottom"]}
+  >
+    <SEO title="Newsletter" />
+    <h2>Newsletter archive</h2>
+    <div class="display_archive">
+      <div class="campaign">
+        08/01/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=2ad8e09938"
+          title="REEEM newsletter, July 2019"
+        >
+          REEEM newsletter, July 2019
+        </a>
+      </div>
+      <div class="campaign">
+        06/28/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=da83fd1c5f"
+          title="REEEM newsletter, June 2019"
+        >
+          REEEM newsletter, June 2019
+        </a>
+      </div>
+      <div class="campaign">
+        05/14/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=bb9625f960"
+          title="REEEM newsletter, May 2019"
+        >
+          REEEM newsletter, May 2019
+        </a>
+      </div>
+      <div class="campaign">
+        04/23/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=9c85dd3fa6"
+          title="REEEM newsletter, April 2019"
+        >
+          REEEM newsletter, April 2019
+        </a>
+      </div>
+      <div class="campaign">
+        03/26/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=7bd8161af0"
+          title="REEEM newsletter, March 2019"
+        >
+          REEEM newsletter, March 2019
+        </a>
+      </div>
+      <div class="campaign">
+        02/08/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=cf226f551f"
+          title="REEEM newsletter, February 2019"
+        >
+          REEEM newsletter, February 2019
+        </a>
+      </div>
+      <div class="campaign">
+        01/11/2019 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=5b31741f50"
+          title="REEEM newsletter, December 2018"
+        >
+          REEEM newsletter, December 2018
+        </a>
+      </div>
+      <div class="campaign">
+        12/18/2018 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=25a6d2dee1"
+          title="REEEM newsletter, November 2018"
+        >
+          REEEM newsletter, November 2018
+        </a>
+      </div>
+      <div class="campaign">
+        11/14/2018 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=fc87e17782"
+          title="REEEM newsletter, October 2018"
+        >
+          REEEM newsletter, October 2018
+        </a>
+      </div>
+      <div class="campaign">
+        10/10/2018 -{" "}
+        <a
+          href="http://us13.campaign-archive.com/?u=ee72f5070843a17245176b969&amp;id=0f9afdd572"
+          title="REEEM newsletter, September 2018"
+        >
+          REEEM newsletter, September 2018
+        </a>
+      </div>
+    </div>
   </Layout>
 )
