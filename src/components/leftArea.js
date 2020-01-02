6f55be7fed1585ba8788bcd9db01e8d97e4bff69
olavurellefsen <oe@tokni.com>
2020-01-02 22:52:05 +0000

    Show double arrows for sidebars on narrow screens

--- src/components/leftArea.js
+++ src/components/leftArea.js
@@ -1,87 +1,144 @@
+import React, { useState } from "react"
 import { Link } from "gatsby"
-import styled from "styled-components"
-import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import styled, { css } from "styled-components"
 import tickmark from "../images/tickmark.png"
 import latestDeliverables from "./latestDeliverables"
+import { media } from "./mediaTemplate"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTwitter } from "@fortawesome/free-brands-svg-icons"
+import {
+  faAngleDoubleRight,
+  faAngleDoubleLeft,
+} from "@fortawesome/free-solid-svg-icons"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
-const LeftArea = () => (
-  <LeftAreaContainer>
-    <TopLeftContainer>
-      <TopLeftArea>
-        <FollowStyle>FOLLOW:</FollowStyle>
-        <TwitterStyle href="https://twitter.com/ReeemProject">
-          <FontAwesomeIcon icon={faTwitter} />
-        </TwitterStyle>
-      </TopLeftArea>
-    </TopLeftContainer>
-    <MainLeftContainer>
-      <MainLeftArea>
-        <h4>
-          <ImgStyle src={tickmark} alt="Tickmark" />
-          LATEST DELIVERABLES
-        </h4>
-        {latestDeliverables.map((deliverable, index) => (
-          <p key={index}>
-            <Deliverable href={deliverable.link}>
-              {deliverable.name}
-            </Deliverable>
-          </p>
-        ))}
-        <p>
-          <LinkStyle to="/deliverables-by-theme">
-            See all deliverables HERE
-          </LinkStyle>
-        </p>
-        <HeadingStyle>PROJECT FACT SHEET</HeadingStyle>
-        <p>
-          <b>Acronym</b>: REEEM
-          <br />
-          <b>Title</b>: Role of technologies in an energy efficient economy –
-          model based analysis policy measures and transformation pathways to a
-          sustainable energy system
-          <br />
-          <b>Call</b>: H2020-LCE
-          <br />
-          <b>Funding scheme</b>: RIA – Research and innovation action
-          <br />
-          <b>Grant agreement no.</b>: 691739
-          <br />
-          <b>Duration</b>: 42 Months
-          <br />
-          <b>Start date</b>: February 2016
-          <br />
-          <b>Estimated Project cost</b>: €3,997,458.75
-          <br />
-          <b>Requested EU contribution</b>: €3,997,458.75
-          <br />
-          <b>Total effort</b>: 423.5 Person-months
-          <br />
-          <b>Project coordinator</b>: Mark Howells – Department of Energy
-          Technology, School of Industrial Engineering and Management, Kungliga
-          Tekniska Högskolan (KTH Royal Institute of Technology)
-          <br />
-          <b>Project Officer</b>: Manuela Conconi
-        </p>
+const LeftArea = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <LeftAreaOuterContainer>
+      <LeftAreaButton open={open} onClick={() => setOpen(!open)}>
+        {!open && <FontAwesomeIcon icon={faAngleDoubleRight} />}
+        {open && <FontAwesomeIcon icon={faAngleDoubleLeft} />}
+      </LeftAreaButton>
+      <LeftAreaContainer open={open}>
+        <TopLeftContainer>
+          <TopLeftArea>
+            <FollowStyle open={open}>FOLLOW:</FollowStyle>
+            <TwitterStyle href="https://twitter.com/ReeemProject">
+              <FontAwesomeIcon icon={faTwitter} />
+            </TwitterStyle>
+          </TopLeftArea>
+        </TopLeftContainer>
+        <MainLeftContainer>
+          <MainLeftArea>
+            <h4>
+              <ImgStyle src={tickmark} alt="Tickmark" />
+              LATEST DELIVERABLES
+            </h4>
+            {latestDeliverables.map((deliverable, index) => (
+              <p key={index}>
+                <Deliverable href={deliverable.link}>
+                  {deliverable.name}
+                </Deliverable>
+              </p>
+            ))}
+            <p>
+              <LinkStyle to="/deliverables-by-theme">
+                See all deliverables HERE
+              </LinkStyle>
+            </p>
+            <HeadingStyle>PROJECT FACT SHEET</HeadingStyle>
+            <p>
+              <b>Acronym</b>: REEEM
+              <br />
+              <b>Title</b>: Role of technologies in an energy efficient economy
+              – model based analysis policy measures and transformation pathways
+              to a sustainable energy system
+              <br />
+              <b>Call</b>: H2020-LCE
+              <br />
+              <b>Funding scheme</b>: RIA – Research and innovation action
+              <br />
+              <b>Grant agreement no.</b>: 691739
+              <br />
+              <b>Duration</b>: 42 Months
+              <br />
+              <b>Start date</b>: February 2016
+              <br />
+              <b>Estimated Project cost</b>: €3,997,458.75
+              <br />
+              <b>Requested EU contribution</b>: €3,997,458.75
+              <br />
+              <b>Total effort</b>: 423.5 Person-months
+              <br />
+              <b>Project coordinator</b>: Mark Howells – Department of Energy
+              Technology, School of Industrial Engineering and Management,
+              Kungliga Tekniska Högskolan (KTH Royal Institute of Technology)
+              <br />
+              <b>Project Officer</b>: Manuela Conconi
+            </p>
 
-        <HeadingStyle>EVENTS</HeadingStyle>
-        <p>
-          <LinkStyle to="/event">See previous events HERE</LinkStyle>
-        </p>
-      </MainLeftArea>
-    </MainLeftContainer>
-  </LeftAreaContainer>
-)
+            <HeadingStyle>EVENTS</HeadingStyle>
+            <p>
+              <LinkStyle to="/event">See previous events HERE</LinkStyle>
+            </p>
+          </MainLeftArea>
+        </MainLeftContainer>
+      </LeftAreaContainer>
+    </LeftAreaOuterContainer>
+  )
+}
+
+const LeftAreaOuterContainer = styled.div`
+  positon: relative;
+`
+
+const LeftAreaButton = styled.button`
+  position: absolute;
+  width: 54px;
+  height: 54px;
+  margin: 0 -54px 0 0;
+  border: 0;
+  &:focus {
+    outline: none;
+  }
+  cursor: pointer;
+  background-color: #3b8dbd;
+  color: white;
+  font-size: 2.5rem;
+  display: none;
+  ${media.desktop3`
+    display: block;
+    ${props =>
+      props.open &&
+      css`
+        position: fixed;
+        width: 54px;
+        z-index: 700;
+      `}
+  `}
+`
 
 const LeftAreaContainer = styled.div`
-  flex: 1 0 340px;
+  flex: 1 0 330px;
   color: #777;
   font-size: 1rem;
   margin: 0px;
   line-height: 24px;
   background: #eaeaea;
   padding-left: calc((100vw - 1380px) / 2);
+  ${media.desktop3`
+    display: none;
+    ${props =>
+      props.open &&
+      css`
+        display: block;
+        position: absolute;
+        left: 0;
+      `}
+  `}
 `
 
 const TopLeftContainer = styled.div`
@@ -93,8 +150,8 @@ const TopLeftContainer = styled.div`
 const TopLeftArea = styled.div`
   display: flex;
   justify-content: space-between;
-  min-width: 340px;
-  max-width: 340px;
+  min-width: 330px;
+  max-width: 330px;
   color: white;
   background: #3b8dbd;
   padding: 15px 30px;
@@ -103,6 +160,7 @@ const TopLeftArea = styled.div`
 const FollowStyle = styled.p`
   font-size: 1rem;
   margin: 0;
+  padding-left: ${props => (props.open ? "40px" : "0")};
 `
 
 const TwitterStyle = styled.a`
@@ -121,8 +179,8 @@ const MainLeftContainer = styled.div`
 `
 
 const MainLeftArea = styled.div`
-  min-width: 340px;
-  max-width: 340px;
+  min-width: 330px;
+  max-width: 330px;
   color: #777;
   background: #f0f0f0;
   padding: 30px;
--- src/components/rightArea.js
+++ src/components/rightArea.js
@@ -1,40 +1,88 @@
-import styled from "styled-components"
-import React from "react"
+import React, { useState } from "react"
+import styled, { css } from "styled-components"
 import euFlag from "../images/eu-flag.png"
 import reflex from "../images/reflex.png"
 import setNav from "../images/set-nav.png"
 import medeas from "../images/medeas.png"
+import { media } from "./mediaTemplate"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faAngleDoubleRight,
+  faAngleDoubleLeft,
+} from "@fortawesome/free-solid-svg-icons"
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
-const RightArea = ({ data }) => (
-  <RightAreaContainer>
-    <TopRightContainer>
-      <TopRightArea>MORE</TopRightArea>
-    </TopRightContainer>
-    <MainRightContainer>
-      <MainRightArea>
-        <img src={euFlag} alt="EU flag" />
-        <EuNote>
-          This project has received funding from the European Union’s Horizon
-          2020 research and innovation programme under grant agreement No
-          691739.
-        </EuNote>
-        <h4>REEEM IS PART OF THE LCE21-2015 PROJECT FAMILY</h4>
-        <ProjectLinkStyle href="http://reflex-project.eu/">
-          <ProjectImgStyle src={reflex} alt="Reflex project logo" />
-        </ProjectLinkStyle>
-        <ProjectLinkStyle href="http://www.set-nav.eu/">
-          <ProjectImgStyle src={setNav} alt="SET-Nav project logo" />
-        </ProjectLinkStyle>
-        <ProjectLinkStyle href="http://medeas.eu/#home">
-          <ProjectImgStyle src={medeas} alt="Medeas project logo" />
-        </ProjectLinkStyle>
-      </MainRightArea>
-    </MainRightContainer>
-  </RightAreaContainer>
-)
+const RightArea = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <RightAreaOuterContainer>
+      <RightAreaButton open={open} onClick={() => setOpen(!open)}>
+        {open && <FontAwesomeIcon icon={faAngleDoubleRight} />}
+        {!open && <FontAwesomeIcon icon={faAngleDoubleLeft} />}
+      </RightAreaButton>
+      <RightAreaContainer open={open}>
+        <TopRightContainer>
+          <TopRightArea>MORE</TopRightArea>
+        </TopRightContainer>
+        <MainRightContainer>
+          <MainRightArea>
+            <img src={euFlag} alt="EU flag" />
+            <EuNote>
+              This project has received funding from the European Union’s
+              Horizon 2020 research and innovation programme under grant
+              agreement No 691739.
+            </EuNote>
+            <h4>REEEM IS PART OF THE LCE21-2015 PROJECT FAMILY</h4>
+            <ProjectLinkStyle href="http://reflex-project.eu/">
+              <ProjectImgStyle src={reflex} alt="Reflex project logo" />
+            </ProjectLinkStyle>
+            <ProjectLinkStyle href="http://www.set-nav.eu/">
+              <ProjectImgStyle src={setNav} alt="SET-Nav project logo" />
+            </ProjectLinkStyle>
+            <ProjectLinkStyle href="http://medeas.eu/#home">
+              <ProjectImgStyle src={medeas} alt="Medeas project logo" />
+            </ProjectLinkStyle>
+          </MainRightArea>
+        </MainRightContainer>
+      </RightAreaContainer>
+    </RightAreaOuterContainer>
+  )
+}
+
+const RightAreaOuterContainer = styled.div`
+  positon: relative;
+`
+
+const RightAreaButton = styled.button`
+  position: absolute;
+  width: 54px;
+  height: 54px;
+  margin: 0 0 0 -54px;
+  border: 0;
+  &:focus {
+    outline: none;
+  }
+  cursor: pointer;
+  background-color: #82b965;
+  color: white;
+  font-size: 2.5rem;
+  display: none;
+  ${media.desktop2`
+    display: block;
+    ${props =>
+      props.open &&
+      css`
+        position: fixed;
+        width: 54px;
+        z-index: 700;
+      `}
+  `}
+`
 
 const RightAreaContainer = styled.div`
-  flex: 1 0 260px;
+  flex: 1 0 250px;
   color: #777;
   font-size: 1rem;
   background: #eaeaea;
@@ -42,6 +90,16 @@ const RightAreaContainer = styled.div`
   line-height: 24px;
   height: 1000px;
   padding-right: calc((100vw - 1380px) / 2);
+  ${media.desktop2`
+    display: none;
+    ${props =>
+      props.open &&
+      css`
+        display: block;
+        position: absolute;
+        right: 0;
+      `}
+  `}
 `
 
 const TopRightContainer = styled.div`
@@ -51,8 +109,8 @@ const TopRightContainer = styled.div`
 `
 
 const TopRightArea = styled.div`
-  min-width: 260px;
-  max-width: 260px;
+  min-width: 250px;
+  max-width: 250px;
   color: white;
   background: #82b965;
   padding: 15px 30px;
@@ -65,8 +123,8 @@ const MainRightContainer = styled.div`
 `
 
 const MainRightArea = styled.div`
-  min-width: 260px;
-  max-width: 260px;
+  min-width: 250px;
+  max-width: 250px;
   color: #777;
   background: #f0f0f0;
   padding: 30px;
