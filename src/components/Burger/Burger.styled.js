eb3cf83aeac0eaff6e76c229ef9950060e070457
olavurellefsen <oe@tokni.com>
2020-01-02 22:50:04 +0000

    Add burger menu component

---
+++ src/components/Burger/Burger.styled.js
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+export const StyledBurger = styled.button`
+  position: absolute;
+  top: 36px;
+  right: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 2rem;
+  height: 2rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 600;
+  &:focus { outline: none; }
+  span {
+    width: 2rem;
+    height: 0.25rem;
+    background: ${({ open }) =>
+      open ? '#eaeaea' : 'white'};
+    border-radius: 10px;
+    transition: all 0.3s linear;
+    position: relative;
+    transform-origin: 1px;
+    :first-child {
+      transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
+    }
+    :nth-child(2) {
+      opacity: ${({ open }) => (open ? "0" : "1")};
+      transform: ${({ open }) => (open ? "translateY(0)" : "translateX(0)")};
+    }
+    :nth-child(3) {
+      transform: ${({ open }) => (open ? "rotate(-45deg)" : "rotate(0)")};
+    }
+  }
+`
